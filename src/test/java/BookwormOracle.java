417e8435fa5eb17ec969666e7939978af6f1979e
yxy <charlay_yu@163.com>
2021-07-21 22:08:42 +0800

    beanFactory实现依赖注入

---
+++ src/test/java/BookwormOracle.java
@@ -0,0 +1,12 @@
+public class BookwormOracle implements Oracle{
+    private Encyclopedia encyclopedia;
+
+    public void setEncyclopedia(Encyclopedia encyclopedia){
+        this.encyclopedia = encyclopedia;
+    }
+
+    @Override
+    public String defineMeaningOfLife(){
+        return "Encyclopedias are a waste of money - go see the world instead";
+    }
+}
